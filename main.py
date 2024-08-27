aa0e344e8dbfc0fe892012586464cd4036c59aed
Jacob234 <Jacobbenkell@gmail.com>
2024-08-26 23:23:20 -0400

    B: Black screen showing

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+pygame.init
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 
 def main():
     print("Starting asteroids!")
 
+    while(True):
+        screen.fill(000)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
