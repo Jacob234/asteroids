569a47845c4503ac23b6191d01ed3aeef5c72f60
Jacob234 <Jacobbenkell@gmail.com>
2024-08-27 18:59:13 -0400

    F: Added CircleShape and Player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 pygame.init
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -10,19 +11,23 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 dt = 0
+player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
 
 
 def main():
     print("Starting asteroids!")
 
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        
         screen.fill(000)
+        player.draw(screen)
+        dt = clock.tick(60)/1000
         pygame.display.flip()
-        dt = clock.tick(60)
 
 
 
